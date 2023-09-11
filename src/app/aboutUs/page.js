6c2465bbc5db559488db7f85d02e2eac141b3208
amogh4319 <amogh4319@gmail.com>
2023-09-11 16:41:03 +0530

    linking of pages

--- src/app/aboutUs/page.js
+++ src/app/aboutUs/page.js
@@ -1,6 +1,31 @@
+import Link from "next/link";
+import { Fragment } from "react";
+
+const details = [
+
+    { id : 1, name: 'Yash', role: 'Senior Developer'},
+    
+    { id : 2, name: 'Vaibhav', role: 'Backend Developer'},
+    
+    { id : 3, name: 'Suresh', role: 'Frontend Developer'}
+    
+    ];
+
+
 export default function AboutUsPage() {
     return (
-      <h1>About Us page</h1>
+        <Fragment>
+            <h1>Know about this people...</h1>
+                <ul>
+                    {details.map((user)=>(
+                    <li key={user.id}>
+                        <Link href={`/aboutUs/${user.id}`}>{user.name}</Link>
+                    </li>
+                    ))}
+                    
+                </ul>
+        </Fragment>
+      
       
     )
   }
